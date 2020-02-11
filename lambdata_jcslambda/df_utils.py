7bfe80ca4c66a3a259ba6a9e61228015b50dd906
jcs-lambda <57103874+jcs-lambda@users.noreply.github.com>
2020-02-11 14:44:56 -0600

    formatting

--- lambdata_jcslambda/df_utils.py
+++ lambdata_jcslambda/df_utils.py
@@ -11,36 +11,47 @@ from sklearn.model_selection import train_test_split
 from IPython.display import display
 
 
-def tvt_split(df, target: str='', random_state=None):
+def tvt_split(df, target: str = '', random_state=None):
     """
     Split a pandas dataframe into train, validation, and test sets.
 
     :param df: pandas dataframe, required
-    :param target: name of a column in df which is passed as stratify parameter
-        to sklearn.model_selection.train_test_split(), optional
-        if blank, does not use the stratify parameter
+    :param target: name of a column in df which is passed as stratify 
+        parameter to sklearn.model_selection.train_test_split(), optional
     :param random_state: define the random_state
-    :returns: tuple of 3 dataframes - (train, validation, test)   
+    :returns: tuple of 3 dataframes - (train, validation, test)
     """
     if target != '' and target in df.columns:
         stratify = df[target]
     else:
         stratify = None
 
-    train, test = train_test_split(df, test_size=0.2, stratify=stratify, random_state=random_state)
+    train, test = train_test_split(
+        df,
+        test_size=0.2,
+        stratify=stratify,
+        random_state=random_state
+    )
 
     if stratify is not None:
         stratify = train[target]
 
-    train, val = train_test_split(train, test_size=0.2, stratify=stratify, random_state=random_state)
+    train, val = train_test_split(
+        train,
+        test_size=0.2,
+        stratify=stratify,
+        random_state=random_state
+    )
 
     return train, val, test
 
 
 def extract_date_parts(dataframe, date_column: str, simple=True):
-    assert date_column in df.columns, f'{date_column} not found in dataframe'
+    assert date_column in df.columns, 
+        f'{date_column} not found in dataframe'
     df = dataframe.copy()
-    datetimes = pd.to_datetime(df[date_column], infer_datetime_format=True, errors='coerce')
+    datetimes = pd.to_datetime(
+        df[date_column], infer_datetime_format=True, errors='coerce')
     df['year'] = datetimes.dt.year
     df['month'] = datetimes.dt.month
     df['day'] = datetimes.dt.day
@@ -77,14 +88,16 @@ def describe(dataframe, formatter={'all': lambda x: f'{x}'}):
 def barplot_feat_by_target_eq_class(feature: str, target: str, target_class, dataframe, ylim=0.7, figsize=(9, 6)):
     assert feature in dataframe.columns, f'FEATURE: {feature} NOT FOUND IN DATAFRAME.columns'
     assert target in dataframe.columns, f'TARGET: {target} NOT FOUND IN DATAFRAME.columns'
-    assert target_class in dataframe[target], f'TARGET CLASS: {target_class} NOT FOUND IN DATAFRAME[\'{target}\']'
+    assert target_class in dataframe[
+        target], f'TARGET CLASS: {target_class} NOT FOUND IN DATAFRAME[\'{target}\']'
     fig, ax = plt.subplots(figsize=figsize)
     sns.barplot(
         ax=ax,
         x=dataframe[feature],
         y=dataframe[target] == target_class
     )
-    plt.axhline(y=dataframe[target].value_counts(normalize=True)[target_class], color='red')
+    plt.axhline(y=dataframe[target].value_counts(
+        normalize=True)[target_class], color='red')
     plt.title(f'{feature} by % {target}=={target_class}')
     plt.ylim(top=ylim)
     plt.show()
