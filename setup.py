2118bbe11f6d78dd592e57ac12fe2d64c946e31c
jcs-lambda <57103874+jcs-lambda@users.noreply.github.com>
2020-02-13 11:04:20 -0600

    docstring and requires

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
+"""Create lambdata_jcslambda package."""
+
 from setuptools import find_packages, setup
+
+REQUIRES = [
+    'numpy',
+    'pandas',
+    'ipython',
+    'matplotlib',
+    'seaborn',
+    'ipywidgets',
+]
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -10,11 +21,11 @@ setup(
     author_email="57103874+jcs-lambda@users.noreply.github.com",
     description="Example package for lambda school DS Unit 3",
     long_description=long_description,
-    long_description_content_type="text/markdown", # required if using a md file for long desc
+    long_description_content_type="text/markdown",
     license="MIT",
     url="https://github.com/jcs-lambda/lambdata-jcs-lambda",
     keywords="lambda school",
-    packages=find_packages(), # ["my_lambdata"]
+    packages=find_packages(),
     python_requires = ">=3.5",
-    install_requires = ['numpy', 'pandas', 'ipython', 'matplotlib', 'seaborn']
+    install_requires = REQUIRES,
 )
