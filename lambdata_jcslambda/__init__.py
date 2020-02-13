ab325af86d03254e7e009e66e24b4d788f046b07
jcs-lambda <57103874+jcs-lambda@users.noreply.github.com>
2020-02-13 14:05:50 -0600

    docstring

--- lambdata_jcslambda/__init__.py
+++ lambdata_jcslambda/__init__.py
@@ -1,3 +1,15 @@
-"""
-lambdata - a collection of data science helper functions
+"""A collection of data science helper functions.
+
+Modules:
+df_utils - functions for exploring and manipulating a dataframe
+explorator - class to apply df_utils functions to a single dataframe
+
+Requires:
+pandas
+numpy
+scikit-learn
+matplotlib
+seaborn
+ipython
+ipywidgets
 """
